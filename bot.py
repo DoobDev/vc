4b1a12d784de631da471f3ccee283d3cddd9fbeb
mmatt625 <notmmatt@gmail.com>
2020-05-25 01:04:43 -0500

    cogs lol

--- bot.py
+++ bot.py
@@ -9,7 +9,6 @@ import dbl
 from discord.ext import commands
 # These just came with Doob Bot, if not needed will remove later.
 
-owner_id = "308000668181069824"
 
 # Creates and loads the json file.
 def get_prefix(client, message):
@@ -21,118 +20,26 @@ def get_prefix(client, message):
 client = commands.Bot(command_prefix = get_prefix)
 
 client.remove_command("help")
-
-def __init__(self, client):
-    self.client = client
-    self.token = 'token' # set this to your DBL token
-    self.dblpy = dbl.DBLClient(self.client, self.token, autopost=True) # Autopost will post your guild count every 30 minutes
-    print("uploaded top.gg")
     
 @client.event
 async def on_ready():
     print('VC is online!')
     await client.change_presence(status = discord.Status.online, activity=discord.Game('vc/help for commands. | github.com/mmatt625/vc'))
 
-@client.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.MissingRequiredArgument):
-        embed = discord.Embed(title="Missing Requirement Error [DB10]", description="Pass in all required arguments.", colour=discord.Color.blue())
-
-        await ctx.send(embed=embed)
-
-    
-    if isinstance(error, commands.MissingPermissions):
-        embed = discord.Embed(title="Missing Permissions Error [DB11]", description="You are not able to use this command because you do not have the required permissions.", colour=discord.Color.blue())
-
-        await ctx.send(embed=embed)
-
-# Opens json file then dumps '-'
-@client.event
-async def on_guild_join(self, guild):
-    with open('prefixes.json', 'r') as f:
-        prefixes = json.load(f)
-
-    prefixes[str(guild.id)] = "vc/"
-
-    with open('prefixes.json', 'w') as f:
-        json.dump(prefixes, f, indent=4)
-
-# Removes guild from json file when Doob leaves.
-@client.event
-async def on_guild_remove(self, guild):
-    with open('prefixes.json', 'r') as f:
-        prefixes = json.load(f)
-
-    prefixes.pop(str(guild.id))
-
-    with open('prefixes.json', 'w') as f:
-        json.dump(prefixes, f, indent=4)
-
-
-@client.command(aliases=['vc', 'screenshare', 'ss'])
-async def vcshare(ctx):
-    embed = discord.Embed(title="Screenshare", description="Cilck this link to screenshare", colour=discord.Color.blue())
-
-    embed.add_field(name="Screenshare here", value=f"https://discordapp.com/channels/{ctx.guild.id}/{ctx.author.voice.channel.id}")
-
-    await ctx.send(embed=embed)
-
-@client.command(aliases=["upvote"])
-async def vote(ctx):
-    embed = discord.Embed(title="Vote", description="Vote on VC bot on...", colour=discord.Color.blue())
-
-    embed.add_field(name="Top.gg", value="https://top.gg/bot/706286009771360310/vote")
-
-    await ctx.send(embed=embed)
+@client.command()
+async def load(ctx, extension):
+    print(f'Loaded {extension}')
+    client.load_extension(f'cogs.{extension}')
+    print(f'Loaded {extension}')
 
 @client.command()
-async def ping(ctx):
-    embed = discord.Embed(title="Pong!", description=":ping_pong:", colour=discord.Color.blue())
+async def unload(ctx, extension):
+    print(f'Unloaded {extension}')
+    client.unload_extension(f'cogs.{extension}')
+    print(f'Unloaded {extension}')
 
-    embed.add_field(name="The latency for VC is...", value=f"{round(client.latency * 1000)} ms")
-
-    await ctx.send(embed=embed)
-
-# Changes the prefix (that the user provides.) for the specific server.
-@client.command(aliases=['prefix'])
-@commands.has_permissions(administrator=True)
-async def changeprefix(ctx, prefix):
-    with open('prefixes.json', 'r') as f:
-        prefixes = json.load(f)
-
-    prefixes[str(ctx.guild.id)] = prefix
-
-    with open('prefixes.json', 'w') as f:
-        json.dump(prefixes, f, indent=4)
-
-    embed = discord.Embed(title="An administrator has changed the prefix.", description=f"An administrator has changed the prefix to {prefix}.", colour=discord.Color.blue())
-
-    embed.add_field(name="The prefix has been changed to:", value=prefix)
-    await ctx.send(embed=embed)
-
-@client.command()
-@commands.is_owner()
-async def ownerprefixchange(ctx, prefix):
-    with open('prefixes.json', 'r') as f:
-        prefixes = json.load(f)
-
-    prefixes[str(ctx.guild.id)] = prefix
-
-    with open('prefixes.json', 'w') as f:
-        json.dump(prefixes, f, indent=4)
-
-    embed = discord.Embed(title="An owner of VC has changed the prefix.", description=f"The owner has changed the prefix to {prefix}.", colour=discord.Color.blue())
-
-    embed.add_field(name="The prefix has been changed to:", value=prefix)
-    await ctx.send(embed=embed)
-
-@client.command(aliases=['helpme', 'commands'])
-async def help(ctx):
-    embed = discord.Embed(title="Command Help", description="All of VC's commands.", colour=discord.Color.blue())
-
-    embed.add_field(name="Check out the docs!", value="https://www.notion.so/doob/VC-Commands-d925cea5bab6425189dd57ff5eb923b3")
-    embed.add_field(name="Join the Support Discord", value="https://discord.gg/ryTYWjD")
-
-    await ctx.send(embed=embed)
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
 
 client.run("token")
