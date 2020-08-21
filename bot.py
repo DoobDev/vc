e668dcf9a5f12ef343ccf814bfe55f959d115322
matt <matt@mmatt.net>
2020-08-21 13:03:55 -0500

    test

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from discord.ext import commands
 token = input("Input your Discord Bot Token.\n")
 
 # Creates and loads the json file.
-def get_prefix(client, message):
+def get_prefix(client, message)
     with open('prefixes.json', 'r') as f:
         prefixes = json.load(f)
     try:
@@ -21,8 +21,8 @@ client = commands.Bot(command_prefix = get_prefix)
 client.remove_command("help")
 
 @client.event
-async def on_ready():
-    print('VC is online!')
+async def on_ready()
+    print('VC is online!'
     await client.change_presence(status = discord.Status.online, activity=discord.Game('vc/help for commands. | github.com/doobdev/vc'))
 
 @client.command()
@@ -38,7 +38,7 @@ async def unload(ctx, extension):
     print(f'Unloaded {extension}')
 
 for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
+    if filename.endswith('.py):
         client.load_extension(f'cogs.{filename[:-3]}')
 
 client.run(token)
